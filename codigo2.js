853f3b2b27347cc6c3a477397c5187057692d459
Jota220395 <57197815+Jota220395@users.noreply.github.com>
2019-11-13 12:35:53 +0100

    Add files via upload

---
+++ codigo2.js
@@ -0,0 +1,32 @@
+$(function() { 
+        $('ul').append("<li>Andalucía</li>");
+          $('ul').append("<li>Aragón</li>"); 
+            $('ul').append("<li>Asturias</li>"); 
+              $('ul').append("<li>Canarias</li>"); 
+                $('ul').append("<li>Cantabria</li>");  
+});
+
+var clone = $("#andalucia").clone();
+
+
+$(document).ready(function() {
+		
+			$("#andalucia").detach();
+			$("#aragon").detach();
+			$("#asturias").detach();
+			$("#canarias").detach();
+			$("#cantabria").detach();
+		});
+
+          
+$(function() { 
+        $('ul').append("<li>Andalucía</li>");
+          $('ul').append("<li>Aragón</li>"); 
+            $('ul').append("<li>Asturias</li>"); 
+              $('ul').append("<li>Canarias</li>"); 
+                $('ul').append("<li>Cantabria</li>");  
+});
+
+$(function() { 
+$("ul").append("<li><span>Isla de Perejil</span></li>")
+});
