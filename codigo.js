0019f8b18f59967f21a4f302e169e2aae94297d8
Jota220395 <57197815+Jota220395@users.noreply.github.com>
2019-11-13 11:45:14 +0100

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,16 @@
+ $(function() {
+        $('#murcia').insertAfter("#madrid"); 
+});
+
+  $(function() {
+        $('#castilla-la-mancha').height(10); 
+});
+   $(function() {
+
+      
+      $(".odd").text().toUpperCase();
+  });
+
+$(function() {
+   $('.even').attr("type", "square");
+   });
